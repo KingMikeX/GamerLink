15815efe316447af2a944ef293c36cea579549d0
Jan153 <jan.wallner.22@heilbronn.dhbw.de>
2025-05-07 14:34:08 +0200

    Pictures Pfad Backend

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -12,9 +12,10 @@ app.include_router(auth_router)
 app.include_router(profile_router)
 app.include_router(tournament_router)
 
+uploads_dir = os.path.join(os.path.dirname(__file__), "uploads", "profile_pictures")
 app.mount(
     "/static/profile_pictures",
-    StaticFiles(directory=os.path.join("uploads", "profile_pictures")),
+    StaticFiles(directory=uploads_dir),
     name="profile_pictures"
 )
 
